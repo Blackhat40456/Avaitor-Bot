1f767eca77a16d2a0240d15b554f50e5132b1325
Blackhat40456 <blackhat404t@gmail.com>
2025-05-03 01:55:31 +0600

    Update mines_bot.py

--- mines_bot.py
+++ mines_bot.py
@@ -18,7 +18,7 @@ def get_box(stars : int = 4):
 async def send_signal():
     M1 = await BOT.sendMessage(ID, '\U0001F6A8 Checking new signal...')
     await asyncio.sleep(random.randint(110, 130)) # 120
-    M2 = await BOT.sendMessage(ID, '\u2705 CONFIRMED ENTRY!\n\nAttempts: 4\n\n\U0001F449 <a href="https://1wzlcz.life/casino/play/1play_1play_mines">Play Here!</a>\n\n' + get_box() + '\n\n\u2705 Tutorial\n\U0001F451 <a href="https://1wnurc.com/casino/list?open=register#mth6">Mines VIP</a>\nPromoCode: <code>ReDHaT</code> use this promo code and get 500% bonus', parse_mode='HTML', reply_markup=single_button('Play Here', 'https://1wzlcz.life/casino/play/1play_1play_mines'), disable_web_page_preview=True)
+    M2 = await BOT.sendMessage(ID, '\u2705 CONFIRMED ENTRY!\n\nAttempts: 4\n\n\U0001F449 <a href="https://1wzlcz.life/casino/play/1play_1play_mines">Play Here!</a>\n\n' + get_box() + '\n\n\u2705 Tutorial\n\U0001F451 <a href="https://1wrjmw.com/casino/list?open=register&p=mth6">Mines VIP</a>\nPromoCode: <code>REDHAT69</code> use this promo code and get 500% bonus', parse_mode='HTML', reply_markup=single_button('Play Here', 'https://1wzlcz.life/casino/play/1play_1play_mines'), disable_web_page_preview=True)
     await asyncio.sleep(random.randint(50, 70)) # 60
     isWIN = bool(random.choice([0] * 20 + [1] * 80))
     M3 = await BOT.sendMessage(ID, '\u2705 <b>GREEENNNNN</b> \u2705' if isWIN else '\U0000274C <b>LOSS</b> \U0000274C', reply_to_message_id=M2.message_id, parse_mode='HTML')
@@ -28,7 +28,7 @@ async def send_signal():
 
 
 async def send_promo():
-    await BOT.sendPhoto(ID, 'static/mines_promo_pic.jpg', 'Create an Account on <a href="https://1wnurc.com/casino/list?open=register#mth6">1WIN</a> & use my promo code - <code>ReDHaT</code> and get 500% bonus\U0001F44D\U0001F4AA\n\nAfter creating your account, send your UID number to <a href="https://t.me/ReDHaT4O4">@ReDHaT4O4</a>\n\n\u26A0\ufe0fDon\'t forget to use my promo code: <code>ReDHaT</code>\n500% Bonus Link: https://1wnurc.com/casino/list?open=register#mth6', parse_mode='HTML', reply_markup=single_button('Create Account', 'https://1wnurc.com/casino/list?open=register#mth6'))
+    await BOT.sendPhoto(ID, 'static/mines_promo_pic.jpg', 'Create an Account on <a href="https://1wrjmw.com/casino/list?open=register&p=mth6">1WIN</a> & use my promo code - <code>REDHAT69</code> and get 500% bonus\U0001F44D\U0001F4AA\n\nAfter creating your account, send your UID number to <a href="https://t.me/ReDHaT4O4">@ReDHaT4O4</a>\n\n\u26A0\ufe0fDon\'t forget to use my promo code: <code>REDHAT69</code>\n500% Bonus Link: https://1wrjmw.com/casino/list?open=register&p=mth6', parse_mode='HTML', reply_markup=single_button('Create Account', 'https://1wrjmw.com/casino/list?open=register&p=mth6'))
 
 
 async def bot_main():
